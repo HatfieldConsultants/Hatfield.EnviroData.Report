12d020530b03455ce011b45aa8852679cebb9709
Daniel <luzhanxun.daniel@gmail.com>
2016-07-28 16:46:26 -0700

    Add test project

---
+++ Test/Hatfield.EnviroData.Report.Test/TestClass.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hatfield.EnviroData.Report.Test
+{
+    public class TestClass
+    {
+        public int IntData { get; set; }
+        public float FloatData { get; set; }
+        public string StringData { get; set; }
+        public bool BooleanData { get; set; }
+    }
+
+    public class TestInvalidClass
+    {
+        public IEnumerable<int> Data { get; set; }
+    }
+
+    public class AnotherTestInvalidClass
+    {
+        public TestInvalidClass Data { get; set; }
+    }
+}
